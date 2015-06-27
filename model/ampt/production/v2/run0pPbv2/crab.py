7180b18a2db5858f15b05f57734fb6b18fbab3a5
Shengquan Tuo <shengquan.tuo@cern.ch>
2015-06-27 02:52:13 -0500

    production cfg for 5.02TeV pPb

---
+++ model/ampt/production/v2/run0pPbv2/crab.py
@@ -0,0 +1,21 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.requestName = 'AMPTstring_June2015_generation_run0pPb5020GeVv2'
+#config.General.workArea = 'crab_projects'
+config.General.transferOutputs = True
+config.General.transferLogs = False
+
+config.JobType.pluginName = 'PrivateMC'
+config.JobType.psetName = 'ampt_StringMelting_pPb5020GeV_cfg.py'
+
+config.Data.primaryDataset = 'MinBias'
+config.Data.splitting = 'EventBased'
+config.Data.unitsPerJob = 200
+NJOBS = 1000  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
+config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
+config.Data.outLFNDirBase = '/store/user/tuos/MC_AMPT/PbPb5TeV/StringMelting/v2/June26/run0pPbv2'
+config.Data.publication = True
+config.Data.publishDataName = 'CRAB3_AMPTstring_June2015_generation_run0pPb5020GeVv2'
+
+config.Site.storageSite = 'T2_US_Vanderbilt'
