053eaa54a3a302ba977ded2921be76ef75b2c67e
Shengquan Tuo <shengquan.tuo@cern.ch>
2017-03-22 13:28:42 -0500

    producing re-centering parameters

---
+++ flowCorr/reCentering/getPtMeanQxy.C
@@ -0,0 +1,243 @@
+#include "TComplex.h"
+#include "TString.h"
+#include <vector>
+#include <TVector3.h>
+#include "TDirectory.h"
+#include "TTree.h"
+#include "TFile.h"
+#include "TH1.h"
+#include "TMath.h"
+#include <fstream>
+#include <iostream>
+
+#define nCentBin 14
+#define nHarmonics 8
+#define nPtBin 20 
+
+void getPtMeanQxy(){
+
+TH1D::SetDefaultSumw2();
+
+  double cBin[nCentBin+1]={0,20,60,70,80,81,82,84,86,88,90,92,94,96,100};
+  double ptBin[nPtBin+1]={0.3, 0.4, 0.5, 0.6, 0.8, 1.0, 1.25, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 5.0, 6.0, 7.0, 8.0, 10.0, 12.0, 14.0, 20.0};
+
+  TH1D* hQhfPlusX[nCentBin][nHarmonics];
+  TH1D* hQhfPlusY[nCentBin][nHarmonics];
+  TH1D* hQhfPlusQ[nCentBin][nHarmonics];
+  TH1D* hQhfMinusX[nCentBin][nHarmonics];
+  TH1D* hQhfMinusY[nCentBin][nHarmonics];
+  TH1D* hQhfMinusQ[nCentBin][nHarmonics];
+  TH1D* hQtrkPlusX[nCentBin][nHarmonics];
+  TH1D* hQtrkPlusY[nCentBin][nHarmonics];
+  TH1D* hQtrkMinusX[nCentBin][nHarmonics];
+  TH1D* hQtrkMinusY[nCentBin][nHarmonics];
+
+  TH1D* hQtrkPtX[nCentBin][nHarmonics][nPtBin];
+  TH1D* hQtrkPtY[nCentBin][nHarmonics][nPtBin];
+
+  TFile* inFile = new TFile("../flowCorr_data_5020_v02Ana_5tevReCent_mar17A_merged.root");
+  for(int ibin=0; ibin<nCentBin; ibin++){
+    for(int iH=0; iH<nHarmonics; iH++){
+      hQhfPlusX[ibin][iH] = (TH1D*)inFile->Get(Form("flowCorr/hQhfPlusX_ibin%d_iH%d",ibin,iH));
+      hQhfPlusY[ibin][iH] = (TH1D*)inFile->Get(Form("flowCorr/hQhfPlusY_ibin%d_iH%d",ibin,iH));
+      hQhfPlusQ[ibin][iH] = (TH1D*)inFile->Get(Form("flowCorr/hQhfPlusQ_ibin%d_iH%d",ibin,iH));
+      hQhfMinusX[ibin][iH] = (TH1D*)inFile->Get(Form("flowCorr/hQhfMinusX_ibin%d_iH%d",ibin,iH));
+      hQhfMinusY[ibin][iH] = (TH1D*)inFile->Get(Form("flowCorr/hQhfMinusY_ibin%d_iH%d",ibin,iH));
+      hQhfMinusQ[ibin][iH] = (TH1D*)inFile->Get(Form("flowCorr/hQhfMinusQ_ibin%d_iH%d",ibin,iH));
+      hQtrkPlusX[ibin][iH] = (TH1D*)inFile->Get(Form("flowCorr/hQtrkPlusX_ibin%d_iH%d",ibin,iH));
+      hQtrkPlusY[ibin][iH] = (TH1D*)inFile->Get(Form("flowCorr/hQtrkPlusY_ibin%d_iH%d",ibin,iH));
+      hQtrkMinusX[ibin][iH] = (TH1D*)inFile->Get(Form("flowCorr/hQtrkMinusX_ibin%d_iH%d",ibin,iH));
+      hQtrkMinusY[ibin][iH] = (TH1D*)inFile->Get(Form("flowCorr/hQtrkMinusY_ibin%d_iH%d",ibin,iH));
+      for(int iPt=0; iPt<nPtBin; iPt++){
+        hQtrkPtX[ibin][iH][iPt] = (TH1D*)inFile->Get(Form("flowCorr/hQtrkPtX_ibin%d_iH%d_iPt%d",ibin,iH,iPt));
+        hQtrkPtY[ibin][iH][iPt] = (TH1D*)inFile->Get(Form("flowCorr/hQtrkPtY_ibin%d_iH%d_iPt%d",ibin,iH,iPt));
+      }
+    }
+  }
+
+  ofstream outvhf;
+  outvhf.open("outvmeanHFPt_v08_corrFlow.txt");
+
+  outvhf<<"double meanQxPlus[nCentBin][nHarmonics] = {";
+  for(int ibin=0; ibin<nCentBin-1; ibin++){
+    outvhf<<"{";
+    for(int iH=0; iH<nHarmonics-1; iH++){
+      outvhf<<hQhfPlusX[ibin][iH]->GetMean()<<", ";
+    }
+    outvhf<<hQhfPlusX[ibin][nHarmonics-1]->GetMean()<<"}, "<<endl;
+  }
+  outvhf<<"{";
+  for(int iH=0; iH<nHarmonics-1; iH++){
+    outvhf<<hQhfPlusX[nCentBin-1][iH]->GetMean()<<", ";
+  }
+  outvhf<<hQhfPlusX[nCentBin-1][nHarmonics-1]->GetMean()<<"}}; "<<endl;
+/////////////
+  outvhf<<"double meanQyPlus[nCentBin][nHarmonics] = {";
+  for(int ibin=0; ibin<nCentBin-1; ibin++){
+    outvhf<<"{";
+    for(int iH=0; iH<nHarmonics-1; iH++){
+      outvhf<<hQhfPlusY[ibin][iH]->GetMean()<<", ";
+    }
+    outvhf<<hQhfPlusY[ibin][nHarmonics-1]->GetMean()<<"}, "<<endl;
+  }
+  outvhf<<"{";
+  for(int iH=0; iH<nHarmonics-1; iH++){
+    outvhf<<hQhfPlusY[nCentBin-1][iH]->GetMean()<<", ";
+  }
+  outvhf<<hQhfPlusY[nCentBin-1][nHarmonics-1]->GetMean()<<"}}; "<<endl;
+///////////
+  outvhf<<"double meanQxMinus[nCentBin][nHarmonics] = {";
+  for(int ibin=0; ibin<nCentBin-1; ibin++){
+    outvhf<<"{";
+    for(int iH=0; iH<nHarmonics-1; iH++){
+      outvhf<<hQhfMinusX[ibin][iH]->GetMean()<<", ";
+    }
+    outvhf<<hQhfMinusX[ibin][nHarmonics-1]->GetMean()<<"}, "<<endl;
+  }
+  outvhf<<"{";
+  for(int iH=0; iH<nHarmonics-1; iH++){
+    outvhf<<hQhfMinusX[nCentBin-1][iH]->GetMean()<<", ";
+  }
+  outvhf<<hQhfMinusX[nCentBin-1][nHarmonics-1]->GetMean()<<"}}; "<<endl;
+////////////
+  outvhf<<"double meanQyMinus[nCentBin][nHarmonics] = {";
+  for(int ibin=0; ibin<nCentBin-1; ibin++){
+    outvhf<<"{";
+    for(int iH=0; iH<nHarmonics-1; iH++){
+      outvhf<<hQhfMinusY[ibin][iH]->GetMean()<<", ";
+    }
+    outvhf<<hQhfMinusY[ibin][nHarmonics-1]->GetMean()<<"}, "<<endl;
+  }
+  outvhf<<"{";
+  for(int iH=0; iH<nHarmonics-1; iH++){
+    outvhf<<hQhfMinusY[nCentBin-1][iH]->GetMean()<<", ";
+  }
+  outvhf<<hQhfMinusY[nCentBin-1][nHarmonics-1]->GetMean()<<"}}; "<<endl;
+/////////
+
+
+  ofstream outvtrk;
+  outvtrk.open("outvmeanTrkPt_v08_corrFlow.txt");
+
+  outvtrk<<"double meanQxTrkPlus[nCentBin][nHarmonics] = {";
+  for(int ibin=0; ibin<nCentBin-1; ibin++){
+    outvtrk<<"{";
+    for(int iH=0; iH<nHarmonics-1; iH++){
+      outvtrk<<hQtrkPlusX[ibin][iH]->GetMean()<<", ";
+    }
+    outvtrk<<hQtrkPlusX[ibin][nHarmonics-1]->GetMean()<<"}, "<<endl;
+  }
+  outvtrk<<"{";
+  for(int iH=0; iH<nHarmonics-1; iH++){
+    outvtrk<<hQtrkPlusX[nCentBin-1][iH]->GetMean()<<", ";
+  }
+  outvtrk<<hQtrkPlusX[nCentBin-1][nHarmonics-1]->GetMean()<<"}}; "<<endl;
+/////////////
+  outvtrk<<"double meanQyTrkPlus[nCentBin][nHarmonics] = {";
+  for(int ibin=0; ibin<nCentBin-1; ibin++){
+    outvtrk<<"{";
+    for(int iH=0; iH<nHarmonics-1; iH++){
+      outvtrk<<hQtrkPlusY[ibin][iH]->GetMean()<<", ";
+    }
+    outvtrk<<hQtrkPlusY[ibin][nHarmonics-1]->GetMean()<<"}, "<<endl;
+  }
+  outvtrk<<"{";
+  for(int iH=0; iH<nHarmonics-1; iH++){
+    outvtrk<<hQtrkPlusY[nCentBin-1][iH]->GetMean()<<", ";
+  }
+  outvtrk<<hQtrkPlusY[nCentBin-1][nHarmonics-1]->GetMean()<<"}}; "<<endl;
+///////////
+  outvtrk<<"double meanQxTrkMinus[nCentBin][nHarmonics] = {";
+  for(int ibin=0; ibin<nCentBin-1; ibin++){
+    outvtrk<<"{";
+    for(int iH=0; iH<nHarmonics-1; iH++){
+      outvtrk<<hQtrkMinusX[ibin][iH]->GetMean()<<", ";
+    }
+    outvtrk<<hQtrkMinusX[ibin][nHarmonics-1]->GetMean()<<"}, "<<endl;
+  }
+  outvtrk<<"{";
+  for(int iH=0; iH<nHarmonics-1; iH++){
+    outvtrk<<hQtrkMinusX[nCentBin-1][iH]->GetMean()<<", ";
+  }
+  outvtrk<<hQtrkMinusX[nCentBin-1][nHarmonics-1]->GetMean()<<"}}; "<<endl;
+////////////
+  outvtrk<<"double meanQyTrkMinus[nCentBin][nHarmonics] = {";
+  for(int ibin=0; ibin<nCentBin-1; ibin++){
+    outvtrk<<"{";
+    for(int iH=0; iH<nHarmonics-1; iH++){
+      outvtrk<<hQtrkMinusY[ibin][iH]->GetMean()<<", ";
+    }
+    outvtrk<<hQtrkMinusY[ibin][nHarmonics-1]->GetMean()<<"}, "<<endl;
+  }
+  outvtrk<<"{";
+  for(int iH=0; iH<nHarmonics-1; iH++){
+    outvtrk<<hQtrkMinusY[nCentBin-1][iH]->GetMean()<<", ";
+  }
+  outvtrk<<hQtrkMinusY[nCentBin-1][nHarmonics-1]->GetMean()<<"}}; "<<endl;
+/////////
+
+  outvtrk<<"double meanQxTrkPt[nCentBin][nHarmonics][nPtBin] = {";
+  for(int ibin=0; ibin<nCentBin-1; ibin++){
+    outvtrk<<"{";
+    for(int iH=0; iH<nHarmonics-1; iH++){
+      outvtrk<<"{";
+      for(int ipt=0; ipt<nPtBin-1; ipt++){
+        outvtrk<<hQtrkPtX[ibin][iH][ipt]->GetMean()<<", "; 
+      }
+      outvtrk<<hQtrkPtX[ibin][iH][nPtBin-1]->GetMean()<<"}, "<<endl;
+    }
+    outvtrk<<"{";
+    for(int iH=0; iH<nPtBin-1; iH++){
+      outvtrk<<hQtrkPtX[ibin][nHarmonics-1][ipt]->GetMean()<<", ";
+    }
+    outvtrk<<hQtrkPtX[ibin][nHarmonics-1][nPtBin-1]->GetMean()<<"}}, "<<endl;
+  }
+  outvtrk<<"{";
+  for(int iH=0; iH<nHarmonics-1; iH++){
+    outvtrk<<"{";
+    for(int ipt=0; ipt<nPtBin-1; ipt++){
+      outvtrk<<hQtrkPtX[nCentBin-1][iH][ipt]->GetMean()<<", ";
+    }
+    outvtrk<<hQtrkPtX[nCentBin-1][iH][nPtBin-1]->GetMean()<<"}, "<<endl;
+  }
+  outvtrk<<"{";
+  for(int iH=0; iH<nPtBin-1; iH++){
+    outvtrk<<hQtrkPtX[nCentBin-1][nHarmonics-1][ipt]->GetMean()<<", ";
+  }
+  outvtrk<<hQtrkPtX[nCentBin-1][nHarmonics-1][nPtBin-1]->GetMean()<<"}}}; "<<endl;
+
+/////////
+
+  outvtrk<<"double meanQyTrkPt[nCentBin][nHarmonics][nPtBin] = {";
+  for(int ibin=0; ibin<nCentBin-1; ibin++){
+    outvtrk<<"{";
+    for(int iH=0; iH<nHarmonics-1; iH++){
+      outvtrk<<"{";
+      for(int ipt=0; ipt<nPtBin-1; ipt++){
+        outvtrk<<hQtrkPtY[ibin][iH][ipt]->GetMean()<<", ";
+      }
+      outvtrk<<hQtrkPtY[ibin][iH][nPtBin-1]->GetMean()<<"}, "<<endl;
+    }
+    outvtrk<<"{";
+    for(int iH=0; iH<nPtBin-1; iH++){
+      outvtrk<<hQtrkPtY[ibin][nHarmonics-1][ipt]->GetMean()<<", ";
+    }
+    outvtrk<<hQtrkPtY[ibin][nHarmonics-1][nPtBin-1]->GetMean()<<"}}, "<<endl;
+  }
+  outvtrk<<"{";
+  for(int iH=0; iH<nHarmonics-1; iH++){
+    outvtrk<<"{";
+    for(int ipt=0; ipt<nPtBin-1; ipt++){
+      outvtrk<<hQtrkPtY[nCentBin-1][iH][ipt]->GetMean()<<", ";
+    }
+    outvtrk<<hQtrkPtY[nCentBin-1][iH][nPtBin-1]->GetMean()<<"}, "<<endl;
+  }
+  outvtrk<<"{";
+  for(int iH=0; iH<nPtBin-1; iH++){
+    outvtrk<<hQtrkPtY[nCentBin-1][nHarmonics-1][ipt]->GetMean()<<", ";
+  }
+  outvtrk<<hQtrkPtY[nCentBin-1][nHarmonics-1][nPtBin-1]->GetMean()<<"}}}; "<<endl;
+
+
+}
+
