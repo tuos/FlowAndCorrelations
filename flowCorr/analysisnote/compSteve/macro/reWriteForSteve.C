7bd20e12d9f1691bf22324385bc4e2e96aff28d9
Shengquan Tuo <shengquan.tuo@cern.ch>
2017-03-29 13:06:58 -0500

    add numerator

--- flowCorr/analysisnote/compSteve/macro/reWriteForSteve.C
+++ flowCorr/analysisnote/compSteve/macro/reWriteForSteve.C
@@ -4,6 +4,8 @@ reWriteForSteve(){
   const int nFile=5;
   const int nCent=10;
   const int nPt=20;
+
+// Den
   char inFilesD[nFile][200]={"outchi422Den_corrFlow.txt","outchi523Den_corrFlow.txt","outchi6222Den_corrFlow.txt","outchi633Den_corrFlow.txt","outchi7223Den_corrFlow.txt"};
   char outFilesD[nFile][nCent][200]={
 {"D24_0_5.txt", "D24_5_10.txt","D24_10_15.txt","D24_15_20.txt","D24_20_25.txt","D24_25_30.txt","D24_30_35.txt","D24_35_40.txt","D24_40_50.txt","D24_50_60.txt"},
@@ -29,7 +31,33 @@ reWriteForSteve(){
     inFile.close();
   }
 
+// Num
+  char inFilesN[nFile][200]={"outchi422Num_corrFlow.txt","outchi523Num_corrFlow.txt","outchi6222Num_corrFlow.txt","outchi633Num_corrFlow.txt","outchi7223Num_corrFlow.txt"};
+  char outFilesN[nFile][nCent][200]={
+{"N42_0_5.txt", "N42_5_10.txt","N42_10_15.txt","N42_15_20.txt","N42_20_25.txt","N42_25_30.txt","N42_30_35.txt","N42_35_40.txt","N42_40_50.txt","N42_50_60.txt"},
+{"N523_0_5.txt", "N523_5_10.txt","N523_10_15.txt","N523_15_20.txt","N523_20_25.txt","N523_25_30.txt","N523_30_35.txt","N523_35_40.txt","N523_40_50.txt","N523_50_60.txt"},
+{"N62_0_5.txt", "N62_5_10.txt","N62_10_15.txt","N62_15_20.txt","N62_20_25.txt","N62_25_30.txt","N62_30_35.txt","N62_35_40.txt","N62_40_50.txt","N62_50_60.txt"},
+{"N63_0_5.txt", "N63_5_10.txt","N63_10_15.txt","N63_15_20.txt","N63_20_25.txt","N63_25_30.txt","N63_30_35.txt","N63_35_40.txt","N63_40_50.txt","N63_50_60.txt"},
+{"N723_0_5.txt", "N723_5_10.txt","N723_10_15.txt","N723_15_20.txt","N723_20_25.txt","N723_25_30.txt","N723_30_35.txt","N723_35_40.txt","N723_40_50.txt","N723_50_60.txt"}
+};
+  for(int i=0; i<nFile; i++){
+    ifstream inFile;
+    inFile.open(inFilesN[i]);
+    for(int j=0; j<nCent; j++){
+      ofstream outFile;
+      outFile.open(outFilesN[i][j]);
+      inFile>>tmp; inFile>>tmp;
+      for(int k=0; k<nPt; k++){
+        inFile>>tmp; inFile>>tmp; inFile>>mPt; inFile>>tmp; inFile>>value; inFile>>err;
+        if(k<16)
+          outFile<<mPt<<"     "<<value<<"     "<<err<<endl;
+      }
+      outFile.close();
+    }
+    inFile.close();
+  }
 
+// Chi
 
 }
 
