1b2edcb8784718f5e699ab128bcd1f9300e381a3
Shengquan <shengquan.tuo@cern.ch>
2020-09-22 17:36:58 -0500

    up with stat. error for cl

---
+++ healpy/isotropic/withStatUncertainty/analysis_iso_ClError.py
@@ -0,0 +1,159 @@
+
+import healpy as hp
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import scipy.special as spc
+import math
+import matplotlib as mpl
+from scipy.special import lpmn
+import scipy.integrate as integrate
+from scipy.integrate import quad
+from numpy import sin, cos
+from matplotlib.cm import ScalarMappable
+import random
+# Set the number of sources and the coordinates for the input
+
+
+nsources = int(199999)
+nside = 8
+npix = hp.nside2npix(nside)
+SIZE = 400
+DPI = 100
+# Coordinates and the density field f
+#thetas = np.random.random(nsources) * np.pi
+#phis = np.random.random(nsources) * np.pi * 2.
+
+#fs = np.random.randn(nsources)
+
+'''
+with open("eventFileFullTot_iso.txt") as inputFile2:
+   lines2 = inputFile2.readlines()
+
+thetas2 = []
+phis2 = []
+
+for i in range(nsources):
+   thetas2.append(float(lines2[i+1].split()[1]))
+   phis2.append(float(lines2[i+1].split()[2]))
+
+indices2 = hp.ang2pix(nside, thetas2, phis2)
+'''
+
+cl = []
+clSqr = []
+clErr = []
+for l in range(24):
+   cl.append(0)
+   clSqr.append(0)
+   clErr.append(0)
+
+indices2 = []
+hpxmap2 = np.zeros(npix, dtype = np.float)
+events = 8000
+mult = 2500
+#arr = [[0]*(mult)]*events
+#arr = [ [0]*mult for i in range(events)]
+arr = [[0 for i in range(mult)] for j in range(events)] 
+for i in range(events):
+    if i%50 == 0:
+        print("First loop with i = "+str(i)+', and events = '+str(events))
+    #print(i)
+    #hpxmap2 = np.zeros(npix, dtype = np.float)
+    for k in range(mult):
+       ipix = random.randint(0, npix-1)
+       #if k == 0:
+       #print('i = '+str(i)+'   ipix = '+str(ipix))
+       #hpxmap2[indices2[i]] += 1.0
+       hpxmap2[ipix] += 1
+       #indices2.append(ipix)
+       arr[i][k] = hp.pix2ang(nside,ipix)
+       #hpxmap[ipix] += 1
+       #print('i = '+str(i)+', First loop, angles: '+str(hp.pix2ang(nside,ipix)))
+       #print('i = '+str(i)+', First loop, angles: '+str(arr[i][k]))
+
+
+hpxmapFinal = np.zeros(npix, dtype = np.float)
+for i in range(events): 
+    if i%50 == 0:
+        print('Second loop with i = '+str(i)+', and events = '+str(events))
+    c = np.zeros(24)   
+    hpxmapFinal = np.zeros(npix, dtype = np.float)
+    hpxmap = np.zeros(npix, dtype = np.float)
+    for k in range(mult):
+       #print('arr[i][k] = '+str(arr[i][k]))
+       indices = hp.ang2pix(nside,arr[i][k][0],arr[i][k][1])
+       #print('i = '+str(i)+', indices = '+str(indices))
+       #indices2.append(indices)
+       hpxmap[indices] += 1
+
+    for n in range(npix):
+       if (hpxmap2[n] > 0):
+          hpxmapFinal[n] += (hpxmap[n]/hpxmap2[n]) * (events)
+       #if l == 0:
+       #    print('i = '+str(i)+'   theta = '+str(arr[i][l][0]))
+
+    c = hp.anafast(hpxmapFinal)
+    #print(c)
+    for x in range(len(c)):
+       cl[x] = cl[x] + c[x]
+       clSqr[x] = clSqr[x] + c[x]*c[x]
+
+for y in range(len(cl)):
+    #print(str(y)+'   '+str(cl[y])+"   "+str(clSqr[y]))
+    cl[y] = cl[y] /(1.0*events)
+    clSqr[y] = clSqr[y] /(1.0*events)
+    clErr[y] = math.sqrt((clSqr[y] - cl[y]*cl[y])/(1.0*events))
+    #print(str(y)+'   '+str(cl[y])+'   '+str(clSqr[y])+'   '+str(cl[y]*cl[y]))
+    #clErr[y] = (clSqr[y] - cl[y]*cl[y])
+    print(str(y)+'   '+str(cl[y])+"   "+str(clErr[y]))
+
+#print(cl)
+#print(clErr)
+
+
+plt.yscale('log')
+plt.plot(cl)
+plt.savefig("powerspect_iso2.png")
+#hpxmap2[1] = 50
+#hpxmap2[npix-2] = 150
+
+#hp_smoothed = hp.sphtfunc.smoothing(hpxmap2, fwhm=np.radians(1), iter = 1)
+'''
+hp.mollview(hpxmap2, cmap = cm.jet, xsize = SIZE, min = events*mult/npix*0.9, max = events*mult/npix*1.1, title='Isotropic randomised')
+#fig = plt.gcf()
+#ax = plt.gca()
+#norm = mpl.colors.Normalize(vmin = 50, vmax = 150)
+#image = ax.get_images()[0]
+#cmap = fig.colorbar(image, norm = norm, orientation = 'horizontal')
+
+hp.graticule()
+plt.savefig("map_iso.png", dpi = DPI)
+
+'''
+'''
+cl = []
+for l in range(24):
+   cl.append(0)
+
+i = 0
+events = 100
+for x in range(events):
+ 
+    i += 1
+    j = x+1
+    phis = []
+    thetas = []
+      #print(lines[i+1].split()[0])
+      thetas.append(float(lines[i].split()[1]))
+      phis.append(float(lines[i].split()[2]))
+      i+=1
+
+    indices = hp.ang2pix(nside, thetas, phis)
+    hpxmap = np.zeros(npix, dtype = np.float)
+    
+    for k in range(len(thetas)):
+      hpxmap[indices[k]] += 1.0
+
+'''
+
